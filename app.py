574b51aa97670d59a87a55b9cebeb65c60165ced
Sir Newton <kuminewton@gmil.com>
2022-02-17 01:31:05 +0000

    'deploy'

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ app.config['SQLALCHEMY_BINDS'] = {"kpasec": "sqlite:///kpasec.db", "kpasecarchiv
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.config['SECRET_KEY'] = os.environ.get('KPASEC_APP')
+app.config['SECRET_KEY'] = 'ghd76878tbingfggcee5dr'#os.environ.get('KPASEC_APP')
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
